ba9bd52b083284a908778138276eaf60d6a45bb7
Damian1724 <39019058+Damian1724@users.noreply.github.com>
2018-07-20 20:25:40 -0400

    Update Test_for_BinarySearchTree.py

--- Test/Test_for_BinarySearchTree.py
+++ Test/Test_for_BinarySearchTree.py
@@ -6,12 +6,14 @@ Author: Damian Cruz
 import random
 my_tree=BinarySearchTree()
 my_randoms=[]
-#in the first loop i am generating 10000 random numbers and inserting them into "my_randoms". From 1 to 10000000 with step of 3
+#in the first loop i am generating 10000 random numbers and inserting them into "my_randoms". 
+#From 1 to 10000000 with step of 3
 for i in range(10000):
     my_randoms.append(random.randrange(1,10000000,3))
 #In the second loop i am adding the numbers to the tree 
 for i in my_randoms:
     my_tree.insert(i)    
-# Here i am checking if the number were placed correctly in the tree, the method "contain" should always return true to any number in "my_randoms".
+# Here i am checking if the number were placed correctly in the tree, the method "contain" should always return true 
+#to any number in "my_randoms".
 #For example i chose the 9977 position but any user who use  this code can change the position number.
 output=my_tree.contain(my_randoms[9977])
